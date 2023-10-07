201f5d95c8d777950b699cca33b774ac975e3d4a
Nev113 <nevosangkane446@gmail.com>
2023-10-07 23:30:37 +0700

    menambahkan framework

---
+++ tictactoe-react/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+
+function Square(){
+  return <button className='square'>X</button>
+}
+function App(){
+  return <>
+  <Square />
+  </>
+}
+export default App
