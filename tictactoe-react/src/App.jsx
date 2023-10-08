a76617d23c8b855cc3605f4caae2dd079f20fdb9
Nev113 <nevosangkane446@gmail.com>
2023-10-08 15:15:05 +0700

    penambahan status pemenang dan mengecek apakah membentuk garis

--- tictactoe-react/src/App.jsx
+++ tictactoe-react/src/App.jsx
@@ -1,27 +1,59 @@
 import { useState } from 'react'
 
 
-function Square(){
-  const [value, setValue] = useState('');
-  function HandleClick(){
-    setValue('X')
-  }
-
-  return <button onClick={HandleClick} className='square'>{value}</button>
+function Square({ value, onSquareClick }){
+  return <button onClick={onSquareClick} className='square'>{value}</button>
 }
 export default function Board(){
-  const [squares, setSquare] = useState(Array(9).fill(null))
+  const [squares, setSquare] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
+  function HandleClick(i){
+    if(squares[i] || calculateWinner(squares)) return;
+    const nextSquare = squares.slice();
+    nextSquare[i] = (isXNext) ? 'X' : 'O';
+    setSquare(nextSquare);
+    setIsXNext(!isXNext)
+  }
+
+  const winner = calculateWinner(squares);
+  console.log(winner)
+  let status;
+  if(winner){
+    status = 'Pemenang => ' + winner;
+  }else{
+    status= 'Next Player : ' + ((isXNext) ? 'X' : 'O');
+  }
   return <>
   <div className='board'>
-  <Square />
-  <Square />
-  <Square />
-  <Square />
-  <Square />
-  <Square />
-  <Square />
-  <Square />
-  <Square />
+  <Square value={squares[0]} onSquareClick={() => HandleClick(0)}/>
+  <Square value={squares[1]} onSquareClick={() => HandleClick(1)}/>
+  <Square value={squares[2]} onSquareClick={() => HandleClick(2)}/>
+  <Square value={squares[3]} onSquareClick={() => HandleClick(3)}/>
+  <Square value={squares[4]} onSquareClick={() => HandleClick(4)}/>
+  <Square value={squares[5]} onSquareClick={() => HandleClick(5)}/>
+  <Square value={squares[6]} onSquareClick={() => HandleClick(6)}/>
+  <Square value={squares[7]} onSquareClick={() => HandleClick(7)}/>
+  <Square value={squares[8]} onSquareClick={() => HandleClick(8)}/>
   </div>
+  <p>{status}</p>
   </>
+}
+function calculateWinner(squares){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i ++){
+    let [a, b, c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[c]){
+      return squares[a];
+    }
+  }
+  return false;
 }
